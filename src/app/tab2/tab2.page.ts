07539c4ceebc5bf38156d966208e5929716c0963
Fabian <fabian_guarascio@hotmail.com>
2024-02-21 16:12:58 -0300

    created the tab2 for infinite scroll

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInfiniteScroll, IonInfiniteScrollContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { GiphyApiService } from '@services/giphy-api.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonInfiniteScroll, IonInfiniteScrollContent, IonGrid, IonRow, IonCol]
 })
 export class Tab2Page {
 
-  constructor() {}
+  private apiService = inject(GiphyApiService)
+
+  trendingGifs: any[] = [];
+  offset = 0;
+
+
+  ngOnInit() {
+    this.loadTrendingGIFs();
+  }
+
+  loadTrendingGIFs(event?:any) {
+    this.apiService.getTrendings(this.offset).subscribe((response) => {
+      this.trendingGifs = this.trendingGifs.concat(response.data);
+      this.offset += 25;
+      if (event) {
+        event.target.complete();
+      }
+    });
+  }
+
+  loadMore(event:any) {
+    this.loadTrendingGIFs(event);
+  }
 
 }
